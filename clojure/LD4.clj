a175549a93dff9a253783664ac9cb68a1ea5a5e9
elias-po <elias.popovs@gmail.com>
2019-11-10 03:00:04 +0200

    added task description

--- clojure/LD4.clj
+++ clojure/LD4.clj
@@ -1,3 +1,5 @@
+;; Task: Compute an average of the given students' marks, for the students, whose names end in 'a'.
+
 (let [input {"Inese" 10 "Vasja" 8 "Petja" 4 "Natasha" 7 "Anja" 10 "Lauris" 6 "Sandra" 8 "Krišjanis" 9}]
     (defn get-map [] (map (fn [x] (if (re-matches #".*a$" (key x)) (val x) 0)) input))
 )
