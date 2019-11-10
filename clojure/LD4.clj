d35a859c2904a9e35b270fb08a6ace35f55f4f09
elias-po <elias.popovs@gmail.com>
2019-11-10 02:58:34 +0200

    first assignment on clojure

---
+++ clojure/LD4.clj
@@ -0,0 +1,9 @@
+(let [input {"Inese" 10 "Vasja" 8 "Petja" 4 "Natasha" 7 "Anja" 10 "Lauris" 6 "Sandra" 8 "Krišjanis" 9}]
+    (defn get-map [] (map (fn [x] (if (re-matches #".*a$" (key x)) (val x) 0)) input))
+)
+
+(defn get-divisor [] (count (filter (fn [x](not= x 0)) (seq (get-map)))))
+(println "Avg mark: " (double (/ (reduce + (get-map)) (get-divisor))))
+
+;; or for lesser readability, this line instead of the last two ¯\_(ツ)_/¯
+;; (println "Avg mark: " (double (/ (reduce + (get-map)) ((fn [] (count (filter (fn [x] (not= x 0)) (seq (get-map)))))))))
