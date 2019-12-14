3ec079e57a22a951f0217a97f8e721c9313c544c
elias-po <elias.popovs@gmail.com>
2019-12-14 02:28:16 +0200

    formatting

--- clojure/ld5.clj
+++ clojure/ld5.clj
@@ -8,14 +8,15 @@
     (if
       (and (not= row 0) (not= row (- key 1)) (>= (count input) (substepPos key row)))
       (get input (substepPos key row)))
-    (if (> (count input) (countCycle key)) (encryptRow (subs input (countCycle key)) key row) "")
+    (if (> (count input) (countCycle key))
+      (encryptRow (subs input (countCycle key)) key row))
     )
   )
 
 (defn bootstrapEncryption
   [input key position]
-  (if (< position key) (str (encryptRow (subs input position) key position) (bootstrapEncryption input key (+ position 1)))
-                       "")
+  (if (< position key)
+    (str (encryptRow (subs input position) key position) (bootstrapEncryption input key (+ position 1))))
   )
 
 (defn encrypt
@@ -23,6 +24,6 @@
   (bootstrapEncryption (clojure.string/replace input " " "_") key 0))
 
 
-(let [input "1 3 5 7 9"]
+(let [input "1234567890"]
   (println (encrypt input 3))
   )
